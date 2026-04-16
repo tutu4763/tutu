d3d98dca8d20e126277ed2c409b6754b75b06a2c
tutu4763 <tutu47200@gmail.com>
2026-04-16 10:37:27 -0300

    finalisando atividade

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function mudar_conteudo_elemento() {
 
     document.getElementById("h1").innerHTML = "Hello JavaScript!";
 }
+
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
